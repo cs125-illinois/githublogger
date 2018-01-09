424453a40b5e38da39e6b9f9403a2d7c6535cf8f
Geoffrey Challen <challen@illinois.edu>
2018-01-08 21:00:58 -0600

    Initial commit.

---
+++ server.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+require('dotenv').config()
+const _ = require('lodash')
+const debug = require('debug')('githublogger')
+
+const http = require('http')
+const githubWebhookHandler = require('github-webhook-handler')
+const mongo = require('mongodb').MongoClient
+const bunyan = require('bunyan')
+const moment = require('moment')
+const log = bunyan.createLogger({
+  name: 'githublogger',
+  streams: [
+    {
+      type: 'rotating-file',
+      path: 'logs/githublogger.log',
+      period: '1d',
+      count: 365,
+      level: 'info'
+    }
+  ]
+})
+
+const argv = require('minimist')(process.argv.slice(2))
+const defaults = {
+  port: 8188
+}
+let config = _.extend(_.clone(defaults), argv)
+debug(config)
+
+const webhookHandler = githubWebhookHandler({
+  path: "/",
+  secret: process.env.GITHUB_SECRET
+})
+
+let github
+webhookHandler.on('push', push => {
+  log.info(push)
+
+  push._id = push.id
+  delete(push.id)
+  push.examined = false
+  push.done = false
+  push.received = moment().toDate()
+
+  github.update({ _id: push._id }, push, { upsert: true })
+})
+webhookHandler.on('error', err => { log.debug(err) })
+
+mongo.connect(process.env.MONGO)
+  .then(client => {
+    github = client.db('MPs').collection('github')
+    http.createServer((request, response) => {
+      webhookHandler(request, response, err => {
+        log.debug(`${ request.url } caused error: ${ err }`)
+        response.statusCode = 404
+      })
+    }).listen(config.port)
+  })
+
+// vim: ts=2:sw=2:et:ft=javascript
